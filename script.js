2a4017ea51480c9f1ab0f416ea8a518625a84971
Vanessa Ogno <Vonessa0716@gmail.com>
2024-01-09 13:48:57 -0500

    failing to figure out how to set this project up

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    // Display the overlay and pop-up
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup() {
+    // Hide the overlay and pop-up
+    document.getElementById("overlay").style.display = "none";
+}
