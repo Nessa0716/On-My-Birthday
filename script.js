ca3e460b5af56df28d93f9dfc7e9f47625620388
Vanessa Ogno <Vonessa0716@gmail.com>
2024-01-10 13:47:52 -0500

    idke testing layout?

--- script.js
+++
@@ -1,9 +0,0 @@
-function openPopup() {
-    // Display the overlay and pop-up
-    document.getElementById("overlay").style.display = "block";
-}
-
-function closePopup() {
-    // Hide the overlay and pop-up
-    document.getElementById("overlay").style.display = "none";
-}
